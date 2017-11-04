c148bc0d52daa408c32adca2b8f4723654b4f7bf
fanmingfei <az8641683@163.com>
2017-11-04 14:51:14 +0800

    init

--- jianghu/ViewController.swift
+++ jianghu/ViewController.swift
@@ -7,11 +7,46 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, WKScriptMessageHandler, WKNavigationDelegate {
+    var webView: WKWebView!
+    var popView: WKWebView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let cgrect = CGRect(x: 0, y: Int(UIApplication.shared.statusBarFrame.height), width: Int(view.frame.width), height: Int(view.frame.height));
+        let conifg = WKWebViewConfiguration()
+        conifg.userContentController.add(self as WKScriptMessageHandler, name: "Test")
+        webView = WKWebView(frame: cgrect, configuration: conifg)
+        
+    
+        
+
+        webView.backgroundColor = UIColor(colorLiteralRed: 0, green: 0, blue: 0, alpha: 1)
+        
+        
+        
+        let myURL = URL(string: "http://jianghu.ldustu.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+
+        webView.navigationDelegate = self
+        
+        
+        view.addSubview(webView)
+        
+        popView = WKWebView(frame: cgrect, configuration: conifg)
+        
+        popView.isHidden = true
+        popView.navigationDelegate = self
+        
+        view.addSubview(popView)
+        
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +54,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print("got message: \(message.body)")
+        if message.name == "Test" {
+            let obj = message.body as! NSObject
+            let event = obj.value(forKey: "event") as! String
+            switch(event){
+                case "layer":
+                    popView.isHidden = false;
+                    popView.load(URLRequest(url: URL(string: obj.value(forKey: "url") as! String)!))
+                    break;
+                case "close":
+                    popView.isHidden = true;
+                    popView.loadHTMLString("", baseURL: nil)
+                    break;
+            default:
+                break;
+            }
+        }
+    }
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print(123)
+    }
 
 }
-
